15c5c691c39eef752b9ed7735c72d11f801068f4
Sai Sri Vathsa Vemula <vathsa.vemula@codenation.co.in>
2020-01-30 18:32:17 +0530

    Added: script to add to PyPi swarm

--- setup.py
+++ setup.py
@@ -1,31 +1,25 @@
-from setuptools import setup, find_packages
 
+from distutils.core import setup
 setup(
-    name="swagger-django-generator",
-    version="1.3.0",
-    description="Generate Django code from a Swagger specification",
-    long_description="""
-    This utility parses Swagger specifications and creates `urls.py`, `views.py`
-    and `schemas.py` files that can be dropped into any existing Django
-    application.
-    """,
-    author="Praekelt Consulting",
-    author_email="dev@praekelt.com",
-    license="BSD",
-    url="",
-    packages=find_packages(),
-    install_requires=[],
-    package_data={
-        "templates": ["*.py"]
-    },
-    tests_require=[],
-    classifiers=[
-        "Programming Language :: Python",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-    ],
-    zip_safe=False,
+  name = 'swagger_django_generator',         # How you named your package folder (MyLib)
+  packages = ['./', 'swagger_django_generator', 'swagger_django_generator.templates.django'],   # Chose the same as "name"
+  version = '1.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Convert given spec to django code',   # Give a short description about your library
+  author = 'Sai Sri Vathsa Vemula',                   # Type in your name
+  author_email = 'vathsa.vemula@codenation.co.in',      # Type in your E-Mail
+  url = 'https://github.com/trilogy-group/swagger-django-generator',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/trilogy-group/swagger-django-generator/archive/v0.9.tar.gz',    # I explain this later on
+  keywords = ['swagger', 'spec', 'django', 'converter'],   # Keywords that define your package best
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
